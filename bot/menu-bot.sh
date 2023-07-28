57280ef66e0aab1dac193692939f56ad8802df7b
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-28 21:08:09 +0700

    menu-bot.sh

--- bot/menu-bot.sh
+++ bot/menu-bot.sh
@@ -31,8 +31,8 @@ echo -e " \033[34m│$NC\033[33m                       MENU BOT                 
 echo -e " \033[33m╰══════════════════════════════════════════════════════════╯\e[0m"
 echo -e " \033[32m╭══════════════════════════════════════════════════════════╮\e[0m"
 echo -e " \033[35m│$NC [01]${NC} \033[0;36m Daftar BOT${NC}"
-echo -e " \033[35m│$NC [02]${NC} \033[0;36m Add BOT Notif${NC}"
-echo -e " \033[35m│$NC [03]${NC} \033[0;36m Delet BOT Notif${NC}"
+echo -e " \033[35m│$NC [02]${NC} \033[0;36m Chek User Bot Trojan${NC}"
+echo -e " \033[35m│$NC [03]${NC} \033[0;36m Chek User Bot Vless${NC}"
 echo -e " \033[35m│$NC [0]${NC} \033[0;36m Exit${NC}"
 echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
 echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
@@ -41,8 +41,8 @@ read -p " Select From Options [ 1 - 3 ] : " opt
 echo -e ""
 case $opt in
 1) clear ; wget ${BOT} && chmod +x cupu.sh && ./cupu.sh ; exit ;;
-2) clear ; add-bot-notif ; exit ;;
-3) clear ; del-bot-notif ; exit ;;
+2) clear ; bot-cek-tr ; exit ;;
+3) clear ; bot-cek-vless ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
